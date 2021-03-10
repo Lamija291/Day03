0164187e6cab4eea37c53d4f7a53d926a1662761
Lamija 291 <hadroviclamija291@gmail.com>
2021-03-11 00:36:12 +0100

    added ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+function main () {
+    
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    STUDLYCAPVAR = 10;
+    PRoperCAmelCAse = "A String";
+    tITLEcASEoVER = 9000;
+
+return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver,
+}
+}
+
+console.log(main());
+module.exports = main;
